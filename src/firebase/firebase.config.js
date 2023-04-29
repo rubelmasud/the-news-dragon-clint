88760780437d35ee1f902c11c7e87cae40480e59
MD:Rubrl RM <inforubel251@gmail.com>
2023-04-29 17:58:32 +0600

    firebase secret key

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCIyrplBjfiWi2K_l4Hpaf8AYEKL44MnhQ",
-    authDomain: "the-news-dragon-7e1ca.firebaseapp.com",
-    projectId: "the-news-dragon-7e1ca",
-    storageBucket: "the-news-dragon-7e1ca.appspot.com",
-    messagingSenderId: "585900020320",
-    appId: "1:585900020320:web:147a3b57481917522a2905"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
